32df7e27734586f15e205009e7a4291131df160f
hmallen <allenhm@gmail.com>
2017-10-06 16:40:13 -0400

    Fully functional test file with logging.

--- test_sorting_debug.py
+++ test_sorting_debug.py
@@ -1,14 +1,34 @@
 #!/usr/env python3
 # TO DO:
-# - Matches per unit time
-# - Level 2 market data (?Level 1?)
-# - CSV logging
+# - Limit deque length to selected time frame
 
 import csv
 import datetime
 from collections import deque
 import gdax
+import sys
 import time
+
+print()
+print('1 - BTC')
+print('2 - LTC')
+print('3 - ETH')
+print()
+
+user_product = input('Choose Product: ')
+if user_product == '1':
+    product = 'BTC-USD'
+    print('BTC-USD market selected.')
+elif user_product == '2':
+    product = 'LTC-USD'
+    print('LTC-USD market selected.')
+elif user_product == '3':
+    product = 'ETH-USD'
+    print('ETH-USD market selected.')
+else:
+    print('Incorrect selection. Exiting')
+    sys.exit(0)
+print()
 
 data_length = 1000
 data_length_match = 100
@@ -21,7 +41,7 @@ match_data = deque(maxlen=data_length_match)
 class myWebsocketClient(gdax.WebsocketClient):
     def on_open(self):
         self.url = "wss://ws-feed.gdax.com/"
-        self.products = ["LTC-USD"]
+        self.products = [product]
         self.message_count = 0
         self.deque_count = 0
     def on_message(self, msg):
@@ -56,16 +76,19 @@ class myWebsocketClient(gdax.WebsocketClient):
         print('Closing websocket.')
 
 
+public_client = gdax.PublicClient()
+
 wsClient = myWebsocketClient()
 wsClient.start()
 
 print(wsClient.url, wsClient.products)
 
 print('Accumulating market data. Please wait...')
-while (wsClient.message_count < 1000 and len(match_data) < 1):
+while (wsClient.message_count < 1000):
     time.sleep(1)
 
 print('Beginning analysis.')
+print()
 while (True):
     buy_length = len(buy_data)
     buy_length_index = buy_length - 1
@@ -74,17 +97,10 @@ while (True):
     match_length = len(match_data)
     match_length_index = match_length - 1
     
-    """
     if match_length == 0:
         match_length_index = 0
     else:
         match_length_index = match_length - 1
-    """
-
-    print('----------------------------------------')
-    print('buy_length:   ' + str(buy_length))
-    print('sell_length:  ' + str(sell_length))
-    print('match_length: ' + str(match_length))
 
     buy_tot = 0.0
     sell_tot = 0.0
@@ -98,23 +114,10 @@ while (True):
     for x in range(0, match_length_index):
         match_tot = match_tot + float(match_data[x][1])
     match_avg = match_tot / match_length
-    print('match_avg:    ' + str(match_avg))
-    print('match_tot:    ' + str(match_tot))
-    print('match_length: ' + str(match_length))
-
-    print('buy_avg:   ' + str(buy_avg))
-    print('sell_avg:  ' + str(sell_avg))
-    print('match_avg: ' + str(match_avg))
     
     time_elapsed_buy = float((buy_data[buy_length_index][0] - buy_data[0][0]).total_seconds())
     time_elapsed_sell = float((sell_data[sell_length_index][0] - sell_data[0][0]).total_seconds())
     time_elapsed_match = float((match_data[match_length_index][0] - match_data[0][0]).total_seconds())
-    print('time_elapsed_match: ' + str(time_elapsed_match))
-
-    print('time_elapsed_buy:   ' + str(time_elapsed_buy))
-    print('time_elapsed_sell:  ' + str(time_elapsed_sell))
-    print('time_elapsed_match: ' + str(time_elapsed_match))
-    print()
 
     buy_volrateflow = (60.0 * buy_avg) / time_elapsed_buy
     sell_volrateflow = (60.0 * sell_avg) / time_elapsed_sell
@@ -122,19 +125,55 @@ while (True):
     match_volrateflow = (60.0 * match_avg) / time_elapsed_match
     match_rate = (60.0 * float(match_length)) / time_elapsed_match
 
-    with open('volrateflow_log.csv', 'a', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([datetime.datetime.now(), "{:.2f}".format(buy_volrateflow), "{:.2f}".format(sell_volrateflow), "{:.2f}".format(buysell_differential),
-                             "{:.2f}".format(match_volrateflow), "{:.2f}".format(match_rate)])
+    product_book = public_client.get_product_order_book(product, level=1)
+    product_ticker = public_client.get_product_ticker(product_id=product)
 
+    high_bid = float(product_book['bids'][0][0])
+    high_bid_vol = float(product_book['bids'][0][1])
+    high_bid_amt = high_bid * high_bid_vol
+    low_ask = float(product_book['asks'][0][0])
+    low_ask_vol = float(product_book['asks'][0][1])
+    low_ask_amt = low_ask * low_ask_vol
+
+    market_price = float(product_ticker['price'])
+    day_volume = float(product_ticker['volume'])
+
+    print('----------------------------------------')
+    print('buy_length:   ' + str(buy_length))
+    print('sell_length:  ' + str(sell_length))
+    print('match_length: ' + str(match_length))
+    print('match_avg:    ' + str(match_avg))
+    print('match_tot:    ' + str(match_tot))
+    print('match_length: ' + str(match_length))
+    print()
+    print('buy_avg:   ' + str(buy_avg))
+    print('sell_avg:  ' + str(sell_avg))
+    print('match_avg: ' + str(match_avg))
+    print()
+    print('time_elapsed_match: ' + str(time_elapsed_match))
+    print('time_elapsed_buy:   ' + str(time_elapsed_buy))
+    print('time_elapsed_sell:  ' + str(time_elapsed_sell))
+    print('time_elapsed_match: ' + str(time_elapsed_match))
+    print()
     print('VOLUME RATE FLOW')
     print('Buy:          ' + str(buy_volrateflow) + ' $/min')
     print('Sell:         ' + str(sell_volrateflow) + ' $/min')
     print('Differential: ' + str(buysell_differential) + ' $/min')
     print('Match:        ' + str(match_volrateflow) + ' $/min')
     print('Match Rate:   ' + str(match_rate) + ' matches/min')
+    print()
+    print('MARKET')
+    print('High Bid:     ' + str(high_bid) + ' / Total: ' + '$' + str(high_bid_amt))
+    print('Low Ask:      ' + str(low_ask) + ' / Total: ' + '$' + str(low_ask_amt))
+    print('Market Price: ' + str(market_price) + ' / 24hr Volume: ' + str(day_volume))
     print('----------------------------------------')
     print()
+
+    with open('volrateflow_log.csv', 'a', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([datetime.datetime.now(), "{:.2f}".format(buy_volrateflow), "{:.2f}".format(sell_volrateflow), "{:.2f}".format(buysell_differential),
+                             "{:.2f}".format(match_volrateflow), "{:.2f}".format(match_rate), "{:.2f}".format(high_bid), "{:.2f}".format(high_bid_vol), "{:.2f}".format(high_bid_amt),
+                             "{:.2f}".format(low_ask), "{:.2f}".format(low_ask_vol), "{:.2f}".format(low_ask_amt), "{:.2f}".format(market_price), "{:.2f}".format(day_volume)])
     
     time.sleep(10)
 
